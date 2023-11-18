ef0c3eb96cea09eaf667bb093ebb49c6f32ecb28
Logan Lagrange <lagrangelogan@gmail.com>
2023-11-18 17:18:57 -0500

    modified server.js to sync to server on launch

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require(`./config/connection`)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const {Category, Product, Tag, ProductTag} = require(`./models/`)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({force:false}).then(function(){
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
